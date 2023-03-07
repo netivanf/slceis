55bdd379949411644688e510ca8409fa8aad0873
Netivan Florentino da Rocha <netivanf@yahoo.com.br>
2023-03-06 21:51:59 -0300

    Ajuste_Tela_Login

---
+++ assets/js/script.js
@@ -0,0 +1,10 @@
+function formatar(mascara, documento){
+    let i = documento.value.length;
+    let saida = mascara.substring(0,1);
+    let texto = mascara.substring(i);
+
+    if(texto.substring(0,1) != saida){
+        documento.value +=texto.substring(0,1);
+    }
+
+}
